8671ce35745f3a976003dda364ef1447e9e46e29
sameer-shaikh <sameermubeen4@gmail.com>
2021-11-16 18:54:23 +0500

    another page completed

---
+++ pages/blog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout";
+import Footer from "../components/Footer";
+import BlogBody from "../components/BlogBody";
+
+const blog = () => {
+	return (
+		<Layout>
+			<BlogBody />
+			<Footer />
+		</Layout>
+	);
+};
+
+export default blog;
