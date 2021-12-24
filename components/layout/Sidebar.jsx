3fdca17ffae3486d839af4f2e9a392217c1a3a1e
sameershaikh55 <sameermubeen4@gmail.com>
2021-12-24 22:53:15 +0500

    upated

--- components/layout/Sidebar.jsx
+++ components/layout/Sidebar.jsx
@@ -15,22 +15,22 @@ const Sidebar = ({ isOpen, ClickEvent, isEnglish, setIsEnglish }) => {
 			<ImCross onClick={ClickEvent} className="CrossBTn" />
 			<ul>
 				<li>
-					<Link className="fw-bold" href="road" onClick={ClickEvent}>
+					<Link className="fw-bold" href="/" onClick={ClickEvent}>
 						Home
 					</Link>
 				</li>
 				<li>
-					<Link className="fw-bold" href="" onClick={ClickEvent}>
-						Family
+					<Link className="fw-bold" href="/blogs" onClick={ClickEvent}>
+						Blogs
 					</Link>
 				</li>
 				<li>
-					<Link className="fw-bold" href="" onClick={ClickEvent}>
+					<Link className="fw-bold" href="/about" onClick={ClickEvent}>
 						About
 					</Link>
 				</li>
 				<li>
-					<Link className="fw-bold" href="" onClick={ClickEvent}>
+					<Link className="fw-bold" href="/pricing" onClick={ClickEvent}>
 						Price
 					</Link>
 				</li>
